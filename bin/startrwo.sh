67a1d7451c5e79e59c1f20fb58f21cf16c4c390f
Bryan J Rodriguez <bryan.j.rodriguez@intel.com>
2020-06-30 01:16:59 -0700

    Patch 1.2.1

--- bin/startrwo.sh
+++ bin/startrwo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export UUID=$(uuidgen)
+export UUID=$(docker run -i --rm --entrypoint="" edge/console-alpine:1.0 uuidgen)
 if grep -q localhost /etc/hosts; then echo "" > /dev/null; else echo "127.0.0.1       localhost" >> /etc/hosts; fi
 /usr/local/bin/docker-compose -p rwo -f /opt/rwo/compose/docker-compose.yml up -d

11540e8d8460232c9ade5d96b818bb4ed797e341
Bryan J Rodriguez <bryan.j.rodriguez@intel.com>
2020-07-07 15:40:02 -0700

    Patch 1.2.1

--- bin/stoprwo.sh
+++ bin/stoprwo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker-compose -p rwo -f /opt/rwo/compose/docker-compose.yml down -v
+docker stop rwo_rngd && docker rm rwo_rngd
 
 for x in $(ls /mnt/); do
     umount /mnt/$x >/dev/null
